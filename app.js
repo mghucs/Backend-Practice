d623e26890d734bab685a426c6089a580ef6e74b
Ming Huang <mingh1002@gmail.com>
2022-01-24 16:40:02 -0500

    Completed Promise of posts with axios

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var app = express();
+const axios = require('axios');
 
 app.listen(3000, () => {
     console.log("Hello Me: running on port 3000");
 });
 
-app.get("/url", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+app.use(express.json())
+app.get("/api/ping", (req, res, next) => {
+    res.json({
+        "success": true
+    })
+});
+
+async function GetPosts(url) {
+    return await axios.get(url)
+}
+
+app.get("/api/posts", (req, res, next) => {
+
+    const tag = req.query.tag;
+
+    GetPosts(`https://api.someapi?tag=${tag}`)
+    .then(function (response) {
+        let posts = response.data.posts
+
+        return response.data.posts
+
+    });
+    
 });
